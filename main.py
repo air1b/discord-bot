308e5969b321319100e59c9e1ea2e2a68413c026
arielb <arielbapi@gmail.com>
2022-01-25 19:15:33 +0200

    response from the bot when ping

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import discord
 
-# creer un client
+# Instance du client 
 client = discord.Client()
+
+# decorateur depend de l'instance 
+@client.event
+async def on_ready():
+    print("Le bot est pret.") # pret a executer des commandes sur le server
+
+@client.event
+async def on_message(message):
+    if message.content.lower() == 'ping':
+        await message.channel.send("pong")
+
+
 client.run("OTM1NTU3Mjc5Mjk2NjYzNjQy.YfAXnQ.7U7lHl_RDGfzKC2aOItP9ASD4tg")
 
